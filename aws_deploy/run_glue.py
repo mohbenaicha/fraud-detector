6a1cae892b9d721bef8a235bc8bb5fb014ad3933
mohben <mohamed.benaicha@hotmail.com>
2025-08-29 19:45:11 -0400

    overwrote aws account id with env var;

--- aws_deploy/run_glue.py
+++ aws_deploy/run_glue.py
@@ -1,10 +1,14 @@
 import boto3
+import sys, os
+from dotenv import load_dotenv
+load_dotenv("../../.env")
 
 glue = boto3.client("glue", region_name="us-east-1")  # change region
+AWS_ACCOUNT_ID = os.environ.get("AWS_ACCOUNT_ID")
 
 response = glue.create_job(
     Name="FraudDataProcessorJob",
-    Role="arn:aws:iam::908027394331:role/glue-admin-role",  # TODO: update it with Glue Execution role
+    Role=f"arn:aws:iam::{AWS_ACCOUNT_ID}:role/glue-admin-role",  # TODO: update it with Glue Execution role
     ExecutionProperty={"MaxConcurrentRuns": 1},
     Command={
         "Name": "glueetl",
--- aws_deploy/run_sagemaker_processing.py
+++ aws_deploy/run_sagemaker_processing.py
@@ -1,17 +1,20 @@
 import sys, os
+from dotenv import load_dotenv
 from sagemaker.processing import ScriptProcessor, ProcessingInput, ProcessingOutput
 
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from utils import categorical_features, numeric_features, expanded_numeric_features
 
-ROLE_ARN = "arn:aws:iam::908027394331:role/sagemaker-execution-role"
-PROCESSED_BUCKET = "fraud-processed"
-MODEL_BUCKET = "fraud-model-store"
-DEPS_BUCKET = "pipeline-deps"
+load_dotenv("../../.env")
+AWS_ACCOUNT_ID = os.environ.get("AWS_ACCOUNT_ID")   
+ROLE_ARN = f"arn:aws:iam::{AWS_ACCOUNT_ID}:role/sagemaker-execution-role"
+PROCESSED_BUCKET = os.environ.get("PROCESSED_BUCKET")
+MODEL_BUCKET = os.environ.get("MODEL_BUCKET")
+DEPS_BUCKET = os.environ.get("DEPS_BUCKET")
 
 script_processor = ScriptProcessor(
-    image_uri="908027394331.dkr.ecr.us-east-1.amazonaws.com/fraud-pipeline", # TODO: update it with custom image
+    image_uri=f"{AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/fraud-pipeline", # TODO: update it with custom image
     command=["python3"],
     instance_type="ml.t3.medium",
     instance_count=1,
